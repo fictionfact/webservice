a5dfa941b75f13ad5bff9268aa6a842b7972d2c3
henrywirawan <henrywirawan@ymail.com>
2017-07-27 14:28:43 +0700

    Auth and item controller recode done

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,8 +3,92 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $item = Item::get();
+        return $item;
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new item();
+        $item->item = $request->item;
+        $item->save();
+        return $item;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::find($id);
+        if (empty($item)){
+            return response()->json([
+                'message' => 'Record not found',
+            ], 404);
+        }
+        return $item;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+        if (empty($item)){
+            return response()->json([
+                'message' => 'Record not found',
+            ], 404);
+        }
+        $item->item = $request->item;
+        $item->save();
+        return $item;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+        if (empty($item)){
+            return response()->json([
+                'message' => 'Record not found',
+            ], 404);
+        }
+        $item->delete();
+        return response()->json([
+            'message' => 'Record deleted',
+        ]);
+    }
+
 }
