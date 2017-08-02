da16bb85e807626e6f5a2eb5517562fd03b3e54b
henrywirawan <henrywirawan@ymail.com>
2017-08-02 18:16:23 +0700

    Shop API doc

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,6 +7,36 @@ use App\Models\Shop;
 
 class ShopController extends Controller
 {
+
+/**
+* 
+*    @SWG\Get(
+*        path="/api/v1/shop",
+*        summary="Retrieves the collection of Shop resources.",
+*        produces={"application/json"},
+*        tags={"shop"},
+*        @SWG\Response(
+*            response=200,
+*            description="Shop collection.",
+*            @SWG\Schema(
+*                type="array",
+*                @SWG\Items(ref="#/definitions/shop")
+*                )
+*            ),
+*            @SWG\Response(
+*                response=401,
+*                description="Unauthorized action.",
+*            ),
+*            @SWG\Parameter(
+*                name="Authorization",
+*                in="header",
+*                required=true,
+*                type="string"
+*            )
+*        )
+*/
+
+
     /**
      * Display a listing of the resource.
      *
@@ -51,7 +81,6 @@ class ShopController extends Controller
     }
 
 
-
     /**
      * Update the specified resource in storage.
      *
@@ -71,6 +100,36 @@ class ShopController extends Controller
         $shop->save();
         return $shop;
     }
+
+/**
+*
+*   @SWG\Delete(
+*        path="/api/v1/shop/{id}",
+*        summary="Removes the Shop resource.",
+*        produces={"application/json"},
+*        tags={"shop"},
+*        @SWG\Response(
+*            response=204,
+*            description="Shop resource deleted.",
+*        ),
+*        @SWG\Response(
+*            response=401,
+*            description="Unauthorized action.",
+*        ),
+*        @SWG\Response(
+*            response=404,
+*            description="Resource not found.",
+*        ),
+*        @SWG\Parameter(
+*            name="id",
+*            in="path",
+*            required=true,
+*            type="integer"
+*        )
+*    )
+*/
+
+
 
     /**
      * Remove the specified resource from storage.
